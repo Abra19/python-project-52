1077fa62b3490ac79a018093799a8b963ddbbdd5
Abra19 <evabrarova@gmail.com>
2024-02-26 17:39:36 +0300

    set env

--- build.sh
+++ build.sh
@@ -2,14 +2,10 @@
 # Exit on error
 set -o errexit
 
-# Modify this line as needed for your package manager (pip, poetry, etc.)
-# poetry install
 make install
 
 # Convert static asset files
-# python manage.py collectstatic --no-input
 make static
 
 # Apply any outstanding database migrations
-# python manage.py migrate
 make migrate
