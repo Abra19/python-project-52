35c97acfca7947e9ea79d84e50693a0c908a0cfa
Abra19 <evabrarova@gmail.com>
2024-03-12 19:13:35 +0300

    fix debug  settings

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -8,6 +8,3 @@ class User(AbstractUser):
     """
     first_name = models.CharField(max_length=150)
     last_name = models.CharField(max_length=150)
-
-    def __str__(self):
-        return self.get_full_name()
